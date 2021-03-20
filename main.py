abbda0196575e9b89beed5d12e3d07bab0345d2d
casa98 <cagudeloa@uninorte.edu.co>
2021-03-20 01:15:58 -0500

    Sleep tracker to automatically respond to contacts messages

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 from configparser import ConfigParser
-import time
+from pyrogram.handlers import MessageHandler
 
 config = ConfigParser()
 config.read('config.ini')
@@ -9,18 +9,36 @@ app = Client(
     "my_account",
     config['pyrogram']['api_id'],
     config['pyrogram']['api_hash']
+
 )
 #######################################################################################
 
-target = "Marxcela"
+sleeping = False
 
-with app:
-    for i in range(500):
-        if i%2 == 0:
-            app.send_photo(chat_id=target, photo="./pic.png", caption=f"Mensaje #{i+1} out of 200")
-        else:
-            app.send_message(target, f"**Hola, Esperancita!**\nMensaje #{i+1} out of 200")
-        time.sleep(0.5)
+def dump(client, message):
+    global sleeping
+    # print(message)
+    # Verify it was a private message:
+    if(message["from_user"]["is_contact"] and message["chat"]["id"] > 0):
+        username = message["from_user"]["username"]
+        text = message["text"]
+        # Message myself
+        if(username == "casa98" and (text == "Sleeping" or text == "Awake")):
+            if text == "Sleeping":
+                sleeping = True
+            else:
+                sleeping = False
+            app.send_message(username, f"You're now {text}")
+        
+        # Send automatic messages if I'm sleeping:
+        if sleeping and username != "casa98":
+            name = message["chat"]["first_name"]
+            app.send_message(username, f"Hey, {name}. I am sleeping, my **userbot** sent this message \U0001F648\U0001F64A")
+
+
+app.add_handler(MessageHandler(dump))
+
+app.run()
 
 app.run()
 
