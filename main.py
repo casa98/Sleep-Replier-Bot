63e6e095682150b2ac20599d2f7ba55dd14d60a3
casa98 <cagudeloa@uninorte.edu.co>
2021-03-24 18:31:30 -0500

    Modify bio when I'm sleeping or awake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from configparser import ConfigParser
 from pyrogram.handlers import MessageHandler
+from pyrogram.raw import functions
 
 config = ConfigParser()
 config.read('config.ini')
@@ -15,25 +16,40 @@ app = Client(
 sleeping = False
 
 @app.on_message(filters.private)
-def dump(client, message):
-    global sleeping
+def home(client, message):
     print(message)
 
+    name = message.chat.first_name
     username = message.from_user.username
     text = message.text
+
+    message_from_me(name, username, text)
+
+
+def message_from_me(name, username, text):
+    global sleeping
+    # Did I message myself? (Saved Messages)
     if(username == "casa98" and (text == "Sleeping" or text == "Awake")):
         if text == "Sleeping":
             sleeping = True
+            change_bio("I'm sleeping now.")
         else:
             sleeping = False
+            change_bio("\U0001F1E8\U0001F1F4")  # Colombian flag
         
         # Message myself
         app.send_message(username, f"You're now {text}")
-    
-    # Send automatic message if I'm sleeping (set here your username).
-    if sleeping and username != "casa98":
-        name = message.chat.first_name
-        app.send_message(username, f"Hey, {name}. I am sleeping, my **userbot** sent this message \U0001F648\U0001F64A")
 
+    else:
+        if sleeping:
+            app.send_message(username, f"Hey, {name}.\nI'm sleeping right now, I'll reply when it's the real me \U0001F648\U0001F64A")
+
+
+def change_bio(bio):
+    app.send(
+        functions.account.UpdateProfile(
+            about = bio
+        )
+    )
 
 app.run()
