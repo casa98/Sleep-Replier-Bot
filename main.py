6745ce539a69787472ddccd7defa4c751c65ceb5
casa98 <cagudeloa@uninorte.edu.co>
2021-03-24 22:28:25 -0500

    Edit message instead of senidng a new  one (Saved Messages)

--- main.py
+++ main.py
@@ -20,16 +20,17 @@ def home(client, message):
     print(message)
 
     name = message.chat.first_name
-    username = message.from_user.username
+    message_id = message.message_id
+    user = message.from_user
     text = message.text
 
-    message_from_me(name, username, text)
+    message_from_me(name, user, message_id, text)
 
 
-def message_from_me(name, username, text):
+def message_from_me(name, user, message_id, text):
     global sleeping
     # Did I message myself? (Saved Messages)
-    if(username == "casa98" and (text == "Sleeping" or text == "Awake")):
+    if(user.username == "casa98" and (text == "Sleeping" or text == "Awake")):
         if text == "Sleeping":
             sleeping = True
             change_bio("I'm sleeping now.")
@@ -37,12 +38,16 @@ def message_from_me(name, username, text):
             sleeping = False
             change_bio("\U0001F1E8\U0001F1F4")  # Colombian flag
         
-        # Message myself
-        app.send_message(username, f"You're now {text}")
+        # Edit the message I sent to myself
+        app.edit_message_text(
+            chat_id = "me",
+            message_id = message_id,
+            text = f"You're now **{text}**"
+        )
 
     else:
         if sleeping:
-            app.send_message(username, f"Hey, {name}.\nI'm sleeping right now, I'll reply when it's the real me \U0001F648\U0001F64A")
+            app.send_message(user.id, f"Hey, {name}.\nI'm sleeping right now, I'll reply when it's the real me \U0001F648\U0001F64A")
 
 
 def change_bio(bio):
