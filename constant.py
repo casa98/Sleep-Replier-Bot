b76adfd347caa33842dfedf7ebad213c1dbea789
casa98 <cagudeloa@uninorte.edu.co>
2021-04-16 23:45:09 -0500

    Code refactoring

---
+++ constant.py
@@ -0,0 +1,12 @@
+# Change this value for your username. I'll find a better approach then
+my_user_name = "casa98"
+
+sleeping = ['Sleeping', 'sleeping', 'Sleep', 'sleep']
+awake = ['Awake', 'awake']
+
+current_status = "You're now {}"
+awake_bio = "\U0001F1E8\U0001F1F4"  # Colombian flag
+sleeping_bio = "I'm sleeping now."
+
+sleeping_message_1 = "Hola {}, estoy durmiendo ahora mismo. Te hablo cuando sea el verdadero yo\U0001F609\n\n__Saludos, mi userbot.__"
+# sleeping_message_2 = "Carlos está durmiendo. Soy una máquina que toma el control de su cuenta en las noches.\n\nLe haré llegar tus saludos\U0001F61A\U0001F61A"
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import constant as const
+
 from pyrogram import Client, filters
 from configparser import ConfigParser
 from pyrogram.handlers import MessageHandler
@@ -5,7 +7,6 @@ from pyrogram.raw import functions, types
 
 config = ConfigParser()
 config.read('config.ini')
-
 app = Client(
     "my_account",
     config['pyrogram']['api_id'],
@@ -16,56 +17,62 @@ app = Client(
 sleeping = False
 last_sleeping_id = -1
 last_awake_id = -1
-# I'm sleeping and someone mesaged me, send an automatic message
-# Ignore these isers not to automatically reply if I'm sleeping
 ignore_users = [703659857, 1057813911]
+
+
+# This function will receive only my messages (Saved Messages)
+# Here is where I setup my userbot to determine whether I'm sleeping or not
+@app.on_message(filters.me)
+def my_messages(client, message):
+    global sleeping, last_sleeping_id, last_awake_id
+    message_id = message.message_id
+    print(message)
+
+    # I'm going to sleep
+    if(message.text in const.sleeping):
+        sleeping = True
+        last_sleeping_id = message_id
+        app.delete_messages("me", last_awake_id)
+        change_bio(const.sleeping_bio)
+        hideTimestamp(hide = True)
+        edit_my_message("me", message_id, const.current_status.format("**sleeping**"))
+
+    # I woke up
+    if(message.text in const.awake):
+        sleeping= False
+        last_awake_id = message_id
+        app.delete_messages("me", last_sleeping_id)
+        change_bio(const.awake_bio)
+        hideTimestamp(hide = False)
+        edit_my_message("me", message_id, const.current_status.format("**awake**"))
+
+def edit_my_message(chat_id, message_id, text):
+    app.edit_message_text(
+        chat_id = chat_id,
+        message_id = message_id,
+        text = text
+    )
 
 
 @app.on_message(filters.private)
 def home(client, message):
-    print(message)
+    if(message.from_user.id not in ignore_users):
+        print(message)
 
-    name = message.chat.first_name
-    message_id = message.message_id
-    user = message.from_user
-    text = message.text
+        name = message.chat.first_name
+        user = message.from_user
+        message_id = message.message_id
+        text = message.text
 
-    message_from_me(name, user, message_id, text)
+        message_received(name, user, message_id, text)
 
 
-def message_from_me(name, user, message_id, text):
-    global sleeping, last_sleeping_id, last_awake_id, ignore_users
-    # Did I message myself? (Saved Messages)
-    if(user.username == "casa98" and (text == "Sleeping" or text == "Awake")):
-        # Edit the message I sent to myself
-        app.edit_message_text(
-            chat_id = "me",
-            message_id = message_id,
-            text = f"You're now **{text}**"
-        )
-        
-        if text == "Sleeping":
-            sleeping = True
-            last_sleeping_id = message_id
-            app.delete_messages("me", last_awake_id)
-            change_bio("I'm sleeping now.")
-            hideTimestamp(hide = True)
+def message_received(name, user, message_id, text):
+    global sleeping
 
-        else:
-            sleeping = False
-            last_awake_id = message_id
-            app.delete_messages("me", last_sleeping_id)
-            change_bio("\U0001F1E8\U0001F1F4")  # Colombian flag
-            hideTimestamp(hide = False)
-
-    elif sleeping:
-        if user.id not in ignore_users:
-            ignore_users.append(user.id)
-            app.send_message(user.id, f"Hey {name}, I'm sleeping right now. "
-            "I'll message you when it's the real me \U0001F609\n\n__Regards, my userbot.__")
-        else:
-            # TODO Already replied once. Think of what to do next, or nothing...
-            pass
+    # I received private messages
+    if sleeping:
+        app.send_message(user.id, const.sleeping_message_1.format(name))
 
 
 def change_bio(bio):
@@ -77,6 +84,8 @@ def change_bio(bio):
 
 
 def hideTimestamp(hide):
+    # This function will show/hide last seen to everyone 
+    # FIXME Hide/show only to contacts and make some exceptions if needed
     hideOrShow = types.InputPrivacyValueDisallowAll() if hide else types.InputPrivacyValueAllowAll()
     app.send(
         functions.account.SetPrivacy(
